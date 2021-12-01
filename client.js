4208c30f4a8bd09e4d868323ce83b0bad8ad51e0
Neavy18 <genevieve.gt18@gmail.com>
2021-12-01 16:00:55 -0500

    Add new file containing connect function

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+const connect = function(){
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+  
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+
+  conn.setEncoding("utf8");
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = {
+  connect,
+
+};
